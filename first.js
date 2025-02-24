e594753d2304108d068283229d6e932d14830788
pratiksha kumari  [archu] <pratikshapathakn05@gmail.com>
2025-02-24 10:32:38 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,84 @@
+//INcrement and Decrement operators(a++,++a,a--,--a)
+
+console.log("Hello")
+
+let a= 20;
+console.log(a++, a); //post increment
+
+//console.log(a++,a);
+
+let b=20;
+console.log(++b,b,b++,++b,b,b++,b++,++b,++b);
+           //21 21 21 23 23 23 24 26 27//
+
+
+           let c=20;
+           console.log(c--,c,--c,--c,c--,c,c,c,--c,c--,c--);
+           //20 19 18 17 17 16 16 16 15 15 14//
+
+           let d=20;
+           console.log( ++d,d,--d,d--,d--,d,d,d--,d++,++d,++d,d++,--d);
+           //21 21 20 20 19 18 18 18 17 19 20 20 20//
+
+
+          // logical operator(&&   and,|| or,! not true =(false))
+           
+          
+let e=15;
+
+let f=20;
+
+console.log(a>b && b<c);
+
+let g=20;
+
+let h=20;
+console.log(g===h && h===g);
+
+let i=20;
+
+let j="20";
+
+console.log(i!== j && j == i);
+
+let k=20;
+
+let l="20";
+
+console.log(k== l && l!== k);
+
+let m =20;
+
+let n="20";
+
+console.log(m === n || n ==m);
+
+let o=20;
+
+let p="20";
+
+console.log(! true);
+
+//conditon or ternary operators
+console.log((5 === 5) ? 'a':'b');
+
+
+let 
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+
